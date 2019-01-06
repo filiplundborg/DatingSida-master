51c308559bfe5f849e5b9d8317a38ce27f31fae3
Samuel lindquist <Samme_li22@hotmail.com>
2019-01-06 12:49:18 +0100

    bugg fix

--- DatingSida/Global.asax.cs
+++ DatingSida/Global.asax.cs
@@ -25,6 +25,8 @@ namespace DatingSida
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
         }
     }
 }
