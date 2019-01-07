1986d4164225854fdfb50537c165126b3920cec9
Oskar Ågren <oagren@outlook.com>
2019-01-07 15:53:04 +0100

    Serialisering klar för stunden

--- DatingSida/Models/UserProfileIndexViewModel.cs
+++ DatingSida/Models/UserProfileIndexViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Serialization;
 
 namespace DatingSida.Models
 {
@@ -13,10 +14,13 @@ namespace DatingSida.Models
         public string Gender { get; set; }
         public string Description { get; set; }
         public string Image { get; set; } = @"Images\avatar.png";
-        
 
+        [XmlIgnore]
         public List<Message> Messages { get; set; }
+
+        [XmlIgnore]
         public List<Message> MessagesSent { get; set; }
+       
         public UserProfileIndexViewModel() {
             Messages = new List<Message>();
             MessagesSent = new List<Message>();
