3b7224060b6e2e9ba10f3c9abb0e56bdf4f7f8be
Oskar Ågren <oagren@outlook.com>
2019-01-13 11:20:59 +0100

    bfiwegfiuwqef

--- DatingSida/Models/ViewModel/CarouselViewModel.cs
+++ DatingSida/Models/ViewModel/CarouselViewModel.cs
@@ -10,5 +10,7 @@ namespace DatingSida.Models.ViewModel
         public string Image { get; set; }
         public string UserName { get; set; }
         public string Description { get; set; }
+
+
     }
 }
